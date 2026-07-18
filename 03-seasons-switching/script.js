4fe04d9a606817fcbfd6acc44e63d267066ce0b4
Jairam <jairamtalluri2005@gmail.com>
2026-07-18 10:54:02 +0000

    Add Image Switching project

---
+++ 03-seasons-switching/script.js
@@ -0,0 +1,27 @@
+let seasonSmallImage = document.getElementById("seasonSmallImage");
+let seasonMediumImage = document.getElementById("seasonMediumImage");
+
+let springBtn = document.getElementById("springBtn");
+let summerBtn = document.getElementById("summerBtn");
+let autumnBtn = document.getElementById("autumnBtn");
+let winterBtn = document.getElementById("winterBtn");
+
+function changeSpring() {
+    seasonSmallImage.src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png"
+    seasonMediumImage.src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png"
+}
+
+function changeSummer() {
+    seasonSmallImage.src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png"
+    seasonMediumImage.src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png"
+}
+
+function changeAutumn() {
+    seasonSmallImage.src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png"
+    seasonMediumImage.src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png"
+}
+
+function changeWinter() {
+    seasonSmallImage.src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png"
+    seasonMediumImage.src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png"
+}
