6805454e3c96a2c262786a6b14c3e001f6e81ed8
Jairam <jairamtalluri2005@gmail.com>
2026-07-23 09:13:45 +0000

    Add Color switcher

---
+++ 07-Color-switcher/script.js
@@ -0,0 +1,7 @@
+let bgColorsArray = ["#e75d7c", "#b16cef", "#53cca4", "#efc84d", "#628ef0", "#184b73", "#883e7f", "#ed048b"];
+let button = document.getElementById("button");
+let bgContainer = document.getElementById("bgContainer");
+button.onclick = function() {
+    let randonIndex = Math.ceil(Math.random() * bgColorsArray.length) - 1;
+    bgContainer.style.backgroundColor = bgColorsArray[randonIndex]
+}
